d1af436e4e95c72903bfc653bc3e5399c1c41bfc
sattwik-sahu <sattwik21@iiserb.ac.in>
2022-05-04 12:56:51 +0000

    Week-4 Q2,3,4

--- week-4/pyramid.c
+++ week-4/pyramid.c
@@ -7,22 +7,20 @@
 
 #include <stdio.h>
 
+int absolute(int x) {
+  return (x > 0) ? x : -x;
+}
+
 int main() {
   int n;
   printf("No. of rows? >> ");
   scanf("%d", &n);
 
   for (int row = 0; row < n; row++) {
-    // No. of column to be printed is the n-th odd natural number
     int nCols = 2 * row + 1;
-
-    // Print the spaces
     for (int i = 0; i < (n - row - 1); i++)
       printf(" ");
-
-    // LCV goes from -row...0...+row
     for (int col = -row; col <= row; col++)
-      // Print (nCols - abs(col)) % 10
       printf("%d", (nCols - ((col > 0) ? col : -col)) % 10);
     printf("\n");
   }
